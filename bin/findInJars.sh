2bc207893119a69f1a9177d0eff73ae27eceae9a
Nick Stolwijk <nick.stolwijk@rigd-loxia.nl>
2016-02-03 11:02:01 +0100

    Even wat updates.

--- bin/findInJars.sh
+++ bin/findInJars.sh
@@ -2,8 +2,8 @@
 
 searchterm=$1
 find . -type f -iname "*.jar" -print0 | while IFS= read -r -d $'\0' line; do
-     echo $line
-     jar -tvf $line | grep $searchterm 
-     #echo Manifest search
-     #unzip -p $line META-INF/MANIFEST.MF | grep $searchterm
+	if jar -tvf $line | grep $searchterm; then
+        echo $line
+        jar -tvf $line | grep $searchterm 
+    fi
 done
